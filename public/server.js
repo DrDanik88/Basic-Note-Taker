ac83363d119a9a0b398a39e79e189f7ed41d4ca9
Compass <daniktherrien@gmail.com>
2023-09-04 21:07:49 -0400

    default route created and working in server.js. but the notes route is not working

--- public/server.js
+++ public/server.js
@@ -9,24 +9,33 @@ const uniqid = require("uniqid");
 
 //Path import
 const path = require("path");
-
 //Note import
 const Note = require('./routes/note')
-//Port which the Express.js server will run
-
 //Initialize an instance of Express.js
 const app = express();
+//Port which the Express.js server will run
 const port = 3000;
+
+
+
+
+
+
+
+
+// Define a default route
+// Serve 'index.html' as the default route ('/')
+// the star is a wildcard that will match any route not previously defined
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 
 // Define a route for '/notes'
 app.get('/notes', (req, res) => {
-  res.send('This is the /notes route');
-});
-
-// Define a default route
-app.get('/', (req, res) => {
-  res.send('This is the default route');
-});
+    res.sendFile(path.join(__dirname, 'notes.html'));
+  });
+  
 
 // Start the server
 app.listen(port, () => {
